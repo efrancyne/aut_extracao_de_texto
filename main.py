b67d214323210cb079069fa6c25256a923d40df5
efrancyne <efrancyne13@gmail.com>
2024-09-20 00:51:34 -0300

    etapa de geração automática do arquivo csv concluida

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import fitz  # PyMuPDF
+import pandas as pd
 
 def extrair_texto(pdf_path, pagina_num, bbox):  #função responsavel por extrair o texto
     pagina = fitz.open(pdf_path).load_page(pagina_num)
     return pagina.get_text("text", clip=fitz.Rect(bbox)).strip()
 
 
-pdf_path = #caminho da nota fiscal
+pdf_path = r"C:\Users\efran\OneDrive\Área de Trabalho\estudos_ufal\tea_eletiva\aut_extracao_de_texto\nfe\download (1).pdf"
 pagina_num = 0  #a pag do pdf
 
 # coordenadas de cada elemento
@@ -23,10 +24,23 @@ emitente, cnpj, data_de_emissao, data_de_vencimento, valor = [
 ]
 
 
-'''
+dados = {
+    "Emitente": [emitente],
+    "CNPJ": [cnpj],
+    "Data de Emissão": [data_de_emissao],
+    "Data de Vencimento": [data_de_vencimento],
+    "Valor": [valor]
+}
+
+df = pd.DataFrame(dados)
+
+# Salvar o DataFrame em um arquivo CSV
+df.to_csv('dadosextraidos.csv', mode='a', header=False, index=False, encoding='utf-8')
+
+
 print(f"Emitente: {emitente}")
 print(f"CNPJ: {cnpj}")
 print(f"Data de Emissão: {data_de_emissao}")
 print(f"Data de Vencimento: {data_de_vencimento}")
 print(f"Valor: {valor}")
-'''
+
